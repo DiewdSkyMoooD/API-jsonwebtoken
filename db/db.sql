96fb8e2523f86e17714f268c0aa9ee38f59edf92
Cesar CH <72636030+DiewdSkyMoooD@users.noreply.github.com>
2021-07-27 22:59:22 -0500

    Update db.sql

--- db/db.sql
+++ db/db.sql
@@ -5,11 +5,11 @@ CREATE TABLE System.users(
     nombre VARCHAR (250) NOT NULL ,
     correo VARCHAR (250) NOT NULL ,
     password VARCHAR (250) NOT NULL 
-)
+);
 
 CREATE TABLE System.documents(
     id_documents INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR (250) NOT NULL,
     url VARCHAR (250) NOT NULL,
     id_user INT NOT NULL ,FOREIGN KEY(id_user) REFERENCES users(id)
-)
+);
